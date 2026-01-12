bfe81b2e033137f22bea06d7dacbb98fd339d2e8
RickyRollins <trevorasmith.1324@gmail.com>
2026-01-12 13:20:31 -0500

    style: update hero text

--- components/heros/HeroText.tsx
+++ components/heros/HeroText.tsx
@@ -4,14 +4,11 @@ export default function HeroText(): JSX.Element {
     <>
       <div className="flex flex-col items-center gap-6">
         <p className="uppercase text-sm tracking-[0.4em] text-white font-bold">
-          Creative Developer & 3D Enthusiast
+          Website and mobile app Developer
         </p>
         <h1 className="text-white drop-shadow-sm font-black">
           RICKY <span className="text-white opacity-90">ROLLINS</span>
         </h1>
-        <p className="py-2 text-white max-w-lg m-auto font-medium leading-relaxed text-lg">
-          Crafting immersive digital experiences through 3D web development and creative coding.
-        </p>
         <button className="mt-4">
           Get in touch
         </button>
